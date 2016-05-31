4701b849ba87c799a274621f6fde09609b0e675a
xingyu zhou <zxy@pal-nat186-37-85.itap.purdue.edu>
2016-05-31 15:03:43 -0400

    11

--- button.js
+++ button.js
@@ -1,6 +1,4 @@
 function scroll_messages()
 {
-   <button type="button"
-		onclick="document.getElementById('demo').innerHTML = Date()">
-		Click me to display Date and Time.</button>
+   return "hello";
 }
